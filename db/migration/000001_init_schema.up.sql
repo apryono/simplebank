23546b0aa78b546f66b725c21cafdebf21199b31
Apryono Boangmanalu <apryonoboang@gmail.com>
2023-07-31 10:54:12 +0700

    Adding Checking Account Balance and Unit Test

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -40,6 +40,6 @@ COMMENT ON COLUMN "transfers"."amount" IS 'it must be positive';
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id"); 
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
